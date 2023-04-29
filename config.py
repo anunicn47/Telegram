f4f8257b1248d929c6a4562c9e9501f5ed1ef74f
Ananya <90745373+anunicn47@users.noreply.github.com>
2023-04-29 17:15:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config(object):
     DB_URI = DB_URI.replace("postgres", "postgresql")
     MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001509525202")
     GBAN_LOGS = getenv("GBAN_LOGS", "-1001509525202")
-    SYS_ADMIN = getenv("SYS_ADMIN", "1669178360")
-    DEV_USERS = getenv("DEV_USERS", "1669178360")
+    SYS_ADMIN = getenv("SYS_ADMIN", "6032807619")
+    DEV_USERS = getenv("DEV_USERS", "6032807619")
     LOAD = getenv("LOAD")
     WEBHOOK = False
     SPB_MODE = True
@@ -39,7 +39,7 @@ class Config(object):
     ALLOW_EXCL = True
     CUSTOM_CMD = False
     CHANNEL = getenv("CHANNEL", "puiisupportchat")
-    SUPPORT = getenv("SUPPORT", "TheSupportChat")
+    SUPPORT = getenv("SUPPORT", "puiisupportchat")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
     CASH_API_KEY = getenv("CASH_API_KEY", "https://www.alphavantage.co/support/#api-key")
@@ -51,6 +51,6 @@ class Config(object):
     CF_API_KEY = getenv("CF_API_KEY", "coffehouse.intellivoid.net")
     BOT_API_URL = getenv("BOT_API_URL", "https://api.telegram.org/bot")
     BOT_API_FILE_URL = getenv("BOT_API_FILE_URL", "https://api.telegram.org/file/bot")
-    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-    ZAID_USER = list(map(int, getenv("DEV_USERS", "1669178360").split()))
+    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6032807619").split()))
+    ZAID_USER = list(map(int, getenv("DEV_USERS", "6032807619").split()))
     NO_LOAD = list(map(int, getenv("NO_LOAD", "").split()))
